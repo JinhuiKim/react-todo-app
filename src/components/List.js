257f8a26981be123c313a81ea962088463807832
JinhuiKim <public.jinhui.kim@gmail.com>
2022-08-10 05:44:23 +0900

    Style: 기존 스타일 제거
    TailWindCss로 Todo 앱 스타일링 해주기

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,6 @@
 import React from "react";
 
 export default function List({ todoData, setTodoData }) {
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
-  };
-
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
-
   const handleClick = (id) => {
     let newTodoData = todoData.filter((data) => data.id !== id);
     setTodoData(newTodoData);
@@ -35,16 +18,14 @@ export default function List({ todoData, setTodoData }) {
   return (
     <div>
       {todoData.map((data) => (
-        <div style={getStyle(data.completed)} key={data.id}>
+        <div key={data.id}>
           <input
             type="checkbox"
             defaultChecked={false}
             onChange={() => handleCompleteChange(data.id)}
           />{" "}
           {data.title}
-          <button style={btnStyle} onClick={() => handleClick(data.id)}>
-            x
-          </button>
+          <button onClick={() => handleClick(data.id)}>x</button>
         </div>
       ))}
     </div>
