fa532522da21c27c6265b62c2076b761912d357f
JinhuiKim <public.jinhui.kim@gmail.com>
2022-08-10 21:24:19 +0900

    Style: 목록 드래그 할 때 색상 진하게 변경
    Drag and Drop 기능 추가하기

--- src/components/List.js
+++ src/components/List.js
@@ -32,30 +32,29 @@ export default function List({ todoData, setTodoData }) {
                     {...provided.draggableProps}
                     ref={provided.innerRef}
                     {...provided.dragHandleProps}
+                    className={`${
+                      snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+                    } flex items-center justify-between w-full px-4 py-1 my-1 text-gray-600 bg-gray-100 border rounded`}
                   >
-                    <div className="flex items-center justify-between w-full px-4 py-1 my-1 text-gray-600 bg-gray-100 border rounded">
-                      <div className="items-center">
-                        <input
-                          type="checkbox"
-                          defaultChecked={false}
-                          onChange={() => handleCompleteChange(data.id)}
-                        />{" "}
-                        <span
-                          className={
-                            data.completed ? "line-through" : undefined
-                          }
-                        >
-                          {data.title}
-                        </span>
-                      </div>
-                      <div className="items-center">
-                        <button
-                          className="px-4 py-2 float-right"
-                          onClick={() => handleClick(data.id)}
-                        >
-                          x
-                        </button>
-                      </div>
+                    <div className="items-center">
+                      <input
+                        type="checkbox"
+                        defaultChecked={false}
+                        onChange={() => handleCompleteChange(data.id)}
+                      />{" "}
+                      <span
+                        className={data.completed ? "line-through" : undefined}
+                      >
+                        {data.title}
+                      </span>
+                    </div>
+                    <div className="items-center">
+                      <button
+                        className="px-4 py-2 float-right"
+                        onClick={() => handleClick(data.id)}
+                      >
+                        x
+                      </button>
                     </div>
                   </div>
                 )}
