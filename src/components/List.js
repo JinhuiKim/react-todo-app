1d88759cd24b6deafb326e054cfea1fdac316e03
JinhuiKim <public.jinhui.kim@gmail.com>
2022-08-10 21:17:04 +0900

    Feat: API 이용하여 Drag and Drop 틀 생성
    Drag and Drop 기능 추가하기

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export default function List({ todoData, setTodoData }) {
   const handleClick = (id) => {
@@ -16,31 +17,54 @@ export default function List({ todoData, setTodoData }) {
   };
 
   return (
-    <div>
-      {todoData.map((data) => (
-        <div key={data.id}>
-          <div className="flex items-center justify-between w-full px-4 py-1 my-1 text-gray-600 bg-gray-100 border rounded">
-            <div className="items-center">
-              <input
-                type="checkbox"
-                defaultChecked={false}
-                onChange={() => handleCompleteChange(data.id)}
-              />{" "}
-              <span className={data.completed ? "line-through" : undefined}>
-                {data.title}
-              </span>
-            </div>
-            <div className="items-center">
-              <button
-                className="px-4 py-2 float-right"
-                onClick={() => handleClick(data.id)}
+    <DragDropContext>
+      <Droppable droppableId="todo">
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {todoData.map((data, index) => (
+              <Draggable
+                key={data.id}
+                draggableId={data.id.toString()}
+                index={index}
               >
-                x
-              </button>
-            </div>
+                {(provided, snapshot) => (
+                  <div
+                    {...provided.draggableProps}
+                    ref={provided.innerRef}
+                    {...provided.dragHandleProps}
+                  >
+                    <div className="flex items-center justify-between w-full px-4 py-1 my-1 text-gray-600 bg-gray-100 border rounded">
+                      <div className="items-center">
+                        <input
+                          type="checkbox"
+                          defaultChecked={false}
+                          onChange={() => handleCompleteChange(data.id)}
+                        />{" "}
+                        <span
+                          className={
+                            data.completed ? "line-through" : undefined
+                          }
+                        >
+                          {data.title}
+                        </span>
+                      </div>
+                      <div className="items-center">
+                        <button
+                          className="px-4 py-2 float-right"
+                          onClick={() => handleClick(data.id)}
+                        >
+                          x
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
           </div>
-        </div>
-      ))}
-    </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
