2fb21c9d938087152f1fa78dfb745f884e84d5b5
JinhuiKim <public.jinhui.kim@gmail.com>
2022-08-19 21:36:57 +0900

    Feat: Form 컴포넌트 변경 시 Lists, List 컴포넌트 리렌더링 되지 않도록 React.memo 사용
    React.memo 를 이용한 렌더링 최적화

--- src/components/List.js
+++ src/components/List.js
@@ -1,53 +1,53 @@
 import React from "react";
 
-export const List = ({
-  id,
-  title,
-  completed,
-  todoData,
-  setTodoData,
-  provided,
-  snapshot,
-}) => {
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    setTodoData(newTodoData);
-  };
+const List = React.memo(
+  ({ id, title, completed, todoData, setTodoData, provided, snapshot }) => {
+    console.log("List Component");
 
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) data.completed = !data.completed;
-      return data;
-    });
+    const handleClick = (id) => {
+      let newTodoData = todoData.filter((data) => data.id !== id);
+      setTodoData(newTodoData);
+    };
 
-    setTodoData(newTodoData);
-  };
+    const handleCompleteChange = (id) => {
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) data.completed = !data.completed;
+        return data;
+      });
 
-  return (
-    <div
-      {...provided.draggableProps}
-      ref={provided.innerRef}
-      {...provided.dragHandleProps}
-      className={`${
-        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-      } flex items-center justify-between w-full px-4 py-1 my-1 text-gray-600 bg-gray-100 border rounded`}
-    >
-      <div className="items-center">
-        <input
-          type="checkbox"
-          defaultChecked={false}
-          onChange={() => handleCompleteChange(id)}
-        />{" "}
-        <span className={completed ? "line-through" : undefined}>{title}</span>
+      setTodoData(newTodoData);
+    };
+
+    return (
+      <div
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+        {...provided.dragHandleProps}
+        className={`${
+          snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+        } flex items-center justify-between w-full px-4 py-1 my-1 text-gray-600 bg-gray-100 border rounded`}
+      >
+        <div className="items-center">
+          <input
+            type="checkbox"
+            defaultChecked={false}
+            onChange={() => handleCompleteChange(id)}
+          />{" "}
+          <span className={completed ? "line-through" : undefined}>
+            {title}
+          </span>
+        </div>
+        <div className="items-center">
+          <button
+            className="px-4 py-2 float-right"
+            onClick={() => handleClick(id)}
+          >
+            x
+          </button>
+        </div>
       </div>
-      <div className="items-center">
-        <button
-          className="px-4 py-2 float-right"
-          onClick={() => handleClick(id)}
-        >
-          x
-        </button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+);
+
+export default List;
